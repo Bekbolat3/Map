d0d3f42d39cdf4f1f0d2bf38c7d253c9476039e1
Bekbolat3 <orynbasarbekbolat06@gmail.com>
2025-03-19 23:44:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var map = L.map('map').setView([34, 100], 4);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
 var targetCountries = ["China", "India", "Japan"];
 var currentCountryIndex = 0;
+
 var asiaGeojson = {
   "type": "FeatureCollection",
   "features": [
     {
       "type": "Feature",
-      "properties": {"name": "China"},
+      "properties": { "name": "China" },
       "geometry": {
         "type": "Polygon",
         "coordinates": [[[73,18],[135,18],[135,53],[73,53],[73,18]]]
@@ -17,7 +20,7 @@ var asiaGeojson = {
     },
     {
       "type": "Feature",
-      "properties": {"name": "India"},
+      "properties": { "name": "India" },
       "geometry": {
         "type": "Polygon",
         "coordinates": [[[68,6],[97,6],[97,36],[68,36],[68,6]]]
@@ -25,7 +28,7 @@ var asiaGeojson = {
     },
     {
       "type": "Feature",
-      "properties": {"name": "Japan"},
+      "properties": { "name": "Japan" },
       "geometry": {
         "type": "Polygon",
         "coordinates": [[[129,31],[146,31],[146,46],[129,46],[129,31]]]
@@ -33,6 +36,7 @@ var asiaGeojson = {
     }
   ]
 };
+
 function style(feature) {
   return {
     fillColor: '#ddd',
@@ -43,21 +47,23 @@ function style(feature) {
     fillOpacity: 0.7
   };
 }
+
 function onEachFeature(feature, layer) {
   layer.on('click', function() {
     if (currentCountryIndex >= targetCountries.length) return;
     var clickedCountry = feature.properties.name;
     var targetCountry = targetCountries[currentCountryIndex];
     if (clickedCountry === targetCountry) {
-      layer.setStyle({fillColor: 'green'});
+      layer.setStyle({ fillColor: 'green' });
     } else {
-      layer.setStyle({fillColor: 'red'});
+      layer.setStyle({ fillColor: 'red' });
     }
     currentCountryIndex++;
     updateCountryList();
   });
   layer.bindPopup(feature.properties.name);
 }
+
 function updateCountryList() {
   var html = "";
   for (var i = 0; i < targetCountries.length; i++) {
@@ -69,5 +75,7 @@ function updateCountryList() {
   }
   document.getElementById('country-list').innerHTML = html;
 }
+
 updateCountryList();
-L.geoJSON(asiaGeojson, {style: style, onEachFeature: onEachFeature}).addTo(map);
+
+L.geoJSON(asiaGeojson, { style: style, onEachFeature: onEachFeature }).addTo(map);
